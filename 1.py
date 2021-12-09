6b1dba612b9f203a268fd1f8e3f3a34e9eb7fe93
0423LyqHyx <95857595+0423LyqHyx@users.noreply.github.com>
2021-12-09 20:44:42 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+w = st.number_input('請輸入體重(KG)')
+h = st.number_input('請輸入身高(M)')
+confirm_input = st.button('輸入確認')
+
+bmi = w/(h*h)
+st.write('BMI為', bmi)
+
+if confirm_input:
+    if (bmi < 18):
+        st.print('體重過輕')
+    elif (bmi < 24):
+        st.print('體重正常')
+    elif (bmi < 27):
+        st.print('體重過重')
+    else:
+        st.print('體重肥胖')
+© 2021 GitHub, Inc.
+Terms
+Privacy
