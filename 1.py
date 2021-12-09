11893c0cdc60e9937f85f763ff3d7021b373f969
0423LyqHyx <95857595+0423LyqHyx@users.noreply.github.com>
2021-12-09 20:54:38 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,7 +5,7 @@ h = st.number_input('請輸入身高(M)')
 confirm_input = st.button('輸入確認')
 
 if confirm_input:
-    bmi = w/(h*h)
+    bmi=w/(h*h)
     st.write('BMI為', bmi)
     if (bmi < 18):
         st.print('體重過輕')
