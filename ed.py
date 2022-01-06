b5e64d4e3227e3e5021222e45897d8efca1eb2c6
0423LyqHyx <95857595+0423LyqHyx@users.noreply.github.com>
2022-01-06 20:47:48 +0800

    Update ed.py

--- ed.py
+++ ed.py
@@ -12,18 +12,18 @@ if confirm_input:
     x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
 #if confirm_input: 
     #x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
-    
-confirm_input2 = st.button('輸入確認')
-if confirm_input2:
-    if x==st.session_state.c:
-        st.write("恭喜你中獎了")
-    elif x>st.session_state.c:
-        if x>=st.session_state.end:
-            st.write("輸入不合法,請重新輸入:")
+if confirm_input:
+    confirm_input2 = st.button('輸入確認')
+    if confirm_input2:
+        if x==st.session_state.c:
+            st.write("恭喜你中獎了")
+        elif x>st.session_state.c:
+            if x>=st.session_state.end:
+                st.write("輸入不合法,請重新輸入:")
+            else:
+                st.session_state.end=x
         else:
-            st.session_state.end=x
-    else:
-        if x<=st.session_state.start:
-            st.write("輸入不合法,請重新輸入:")
-        else:
-            st.session_state.start=x
+            if x<=st.session_state.start:
+                st.write("輸入不合法,請重新輸入:")
+            else:
+                st.session_state.start=x
