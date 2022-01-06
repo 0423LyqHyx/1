58320b4d63ee0d41771d9dd922f7d0483deb0011
0423LyqHyx <95857595+0423LyqHyx@users.noreply.github.com>
2022-01-06 20:11:58 +0800

    Update ed.py

--- ed.py
+++ ed.py
@@ -1,6 +1,8 @@
 __author__="teacher"
 import random
 import streamlit as st
+start = session_state.start
+end = session_state.end
 confirm_input = st.button('確認產生答案')
 if confirm_input:
     st.session_state.c = c = random.randint(2,99)
@@ -8,11 +10,10 @@ if confirm_input:
     st.session_state.end = 100
     st.session_state.start,st.session_state.end = 1,100
     st.write('c=', st.session_state.c)
-  
 if confirm_input:
     x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
-    #confirm_input2 = st.button('輸入確認')
-    #if confirm_input2:
+    confirm_input2 = st.button('輸入確認')
+    if confirm_input2:
     if x==st.session_state.c:
         st.write("恭喜你中獎了")
     elif x>st.session_state.c:
